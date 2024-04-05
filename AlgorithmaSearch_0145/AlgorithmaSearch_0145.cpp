6765759685bea7a0b5bdd02b856406caae361e80
Qilqilll <aqilf612@gmail.com>
2024-04-05 10:19:53 +0700

    untuk memasukan data

--- AlgorithmaSearch_0145/AlgorithmaSearch_0145.cpp
+++ AlgorithmaSearch_0145/AlgorithmaSearch_0145.cpp
@@ -16,16 +16,16 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+	cout << "\n-------------------\n";
+	cout << " Enter array elements \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
 }
 
-cout << "\n-------------------\n";
-cout << " Enter array elements \n";
-cout << "\n---------------------\n";
-for (i = 0; i < n; i++)
-{
-	cout << "<" << (i + 1) << "> ";
-	cin >> arr[i];
-}
 
 void LinearSearch()
 {
@@ -67,3 +67,7 @@ void display()
 		cout << arr[i] << " ";
 	cout << endl;
 }
+
+int main()
+{
+	input();
