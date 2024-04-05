eb22b48821836fed3caf8da6da0e78a64cdab125
Qilqilll <aqilf612@gmail.com>
2024-04-05 09:35:06 +0700

    untuk menampilkan dan memasukan nilai tiap array

--- AlgorithmaSearch_0145/AlgorithmaSearch_0145.cpp
+++ AlgorithmaSearch_0145/AlgorithmaSearch_0145.cpp
@@ -14,6 +14,15 @@ void input()
 		if ((n > 0) && (n <= 20))
 			break;
 		else
-			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n"
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+}
+
+cout << "\n-------------------\n";
+cout << "Enter array elements \n";
+cout << "\n---------------------\n";
+for (i = 0; i < n; i++)
+{
+	cout << "<" << (i + 1) << "> ";
+	cin >> arr[i];
 }
